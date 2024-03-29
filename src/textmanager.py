0fcbf27d0c62fd51f8fee89dc06a8abce8dc110e
Beck Boan <beckmboan@gmail.com>
2024-03-28 22:44:44 -0400

    Added beginning of split_nodes_images functionality

--- src/textmanager.py
+++ src/textmanager.py
@@ -1,6 +1,8 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode
 
+import re
+
 
 class TextTypes:
     text = "text"
@@ -42,8 +44,52 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(parts[0], TextTypes.text))
                 new_nodes.append(
                     TextNode(parts[1], text_type))
-                new_nodes.append(TextNode(parts[2], TextTypes.text))
+                new_nodes.append(TextNode(parts[2], text_type))
 
         else:
             new_nodes.append(node)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    # ![text](url)
+    match = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    results = []
+
+    for m in match:
+        text = re.search(r"?<=\[).*?(?=\])", m)
+        url = re.search(r"?<=\().*?(?=\))", m)
+        results.append(tuple([text, url]))
+
+
+def extract_markdown_links(text):
+    # [text](url)
+    match = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    results = []
+
+    for m in match:
+        text = re.search(r"?<=\[).*?(?=\])", m)
+        url = re.search(r"?<=\().*?(?=\))", m)
+        results.append(tuple([text, url]))
+
+
+def split_nodes_images(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            text = node.text
+            images = extract_markdown_images(text)
+            if len(images == 0):
+                new_nodes.append(node)
+            else:
+                end_text = text
+                for image in images:
+                    textsplit = end_text.split(f"![{image[0]}]({image[1]})", 1)
+                    end_text = textsplit[-1]
+        else:
+            if (node.text != ""):
+                new_nodes.append(node)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
