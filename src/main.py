67e3a40764c365d2dc691967754a361f38d5e91a
Beck Boan <beckmboan@gmail.com>
2024-04-01 20:20:53 -0400

    Added page generator function.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import copycontents
 import os
+import pagegenerator
 
 
 def main():
@@ -10,5 +11,8 @@ def main():
     PUBLIC_DIR = os.path.join(ROOT_DIR, "public")
     print(PUBLIC_DIR)
 
-    copycontents.copy(STATIC_DIR, PUBLIC_DIR)
+    copycontents.copycontents(STATIC_DIR, PUBLIC_DIR)
+
+    pagegenerator.generate_page(os.path.join(ROOT_DIR, "content", "index.md"), os.path.join(ROOT_DIR, "template.html"), os.path.join(PUBLIC_DIR, "index.html"))
+
 main()
